1e6518e701f1e6c64b99bcdfcbc28def1f432bb7
OzkanBirak <mcpeozkan@gmail.com>
2025-06-30 01:36:34 +0300

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import userData from '../userData.json';
+import userData from './userData.json';
 import friends from './friends.json';
-import transactions from '../transactions.json';
+import transactions from './transactions.json';
 import './App.css';
 
 const App = () => {
